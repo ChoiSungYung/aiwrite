859aef252de913e2f5e8f9bbf9d7845ddeeea9e3
ChoiSungYung <kenshin753@gmail.com>
2024-12-12 10:04:26 +0900

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,21 @@
 import '../styles/globals.css'
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider } from '@supabase/auth-helpers-react'
+import { useState } from 'react'
 import Layout from '../components/Layout'
 
 function MyApp({ Component, pageProps }) {
+  const [supabaseClient] = useState(() => createClientComponentClient())
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <SessionContextProvider 
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionContextProvider>
   )
 }
 
